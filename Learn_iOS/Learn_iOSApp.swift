9993d8f22aaed3b979f4fbd9a94c0ef3cdbd003b
xie-wang <1473970276@qq.com>
2024-10-01 17:30:18 +0800

    feat: create preject for ios.

---
+++ Learn_iOS/Learn_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_iOSApp.swift
+//  Learn_iOS
+//
+//  Created by 王勰 on 2024/10/1.
+//
+
+import SwiftUI
+
+@main
+struct Learn_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
